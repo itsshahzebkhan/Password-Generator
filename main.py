38c3fae4d342ce63f6d74d7a66a3d7cde55dd013
Shahzeb Khan <shahzeb4325@gmail.com>
2024-09-16 19:37:59 +0530

    Add files via upload
    uploaded Python file

---
+++ main.py
@@ -0,0 +1,51 @@
+# step 1 - to start this project you need to install PyCharm community edition or VS Code
+# step 2 - you need to import the "random, string" packages which are already installed in PyCharm and VS Code
+# step 3 - start coding
+
+
+import random
+import string
+
+def generate_password(min_length, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+
+    if special_characters:
+        characters += special
+
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+
+        elif new_char in special:
+            has_special = True
+
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+
+    return pwd
+
+
+min_length = int(input("Enter the minimum length: "))
+has_number = input("Do you want to have numbers (y/n)? ").lower() == "y"
+has_special = input("Do you want to have special characters (y/n)? ").lower() == "y"
+
+pwd = generate_password(min_length, has_number, has_special)
+print("The generated password is:", pwd)
